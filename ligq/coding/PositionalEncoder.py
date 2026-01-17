fc851fac7b62aa1d313878ad68d57d4dfa0a11af
2ySong <2909368784@qq.com>
2026-01-17 13:19:20 +0800

    re

---
+++ ligq/coding/PositionalEncoder.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import math
+
+class PositionalEncoder(nn.Module):
+    def __init__(self, d_model, max_seq_len):
+        super(PositionalEncoder, self).__init__()
+        self.d_model = d_model
+        pe = torch.zeros(max_seq_len, d_model)
+        for pos in range(max_seq_len):
+            for i in range(0, d_model, 2):
+                pe[pos, i] = math.sin(pos / (10000 ** ((2 * i)/d_model)))
+                pe[pos, i + 1] = math.cos(pos / (10000 ** ((2 * (i + 1))/d_model)))
+        pe = pe.unsqueeze(0)  # Shape: (1, max_seq_len, d_model)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x * math.sqrt(self.d_model)
+        seq_len = x.size(1)
+        x = x + self.pe[:, :seq_len, :]
+        return x
